8a0ce06349d330db5bd75b75c21d9f7d6f4dbde9
aProject2021 <aliciaproyectos2021@gmail.com>
2022-11-30 21:19:05 -0500

    Duracion

--- view/modulo/accion.js
+++ view/modulo/accion.js
@@ -12,13 +12,19 @@ var registrarModulo = () => {
     let val_familia = document.getElementById("cbo-familia").value
     let val_opcion = document.getElementById("cbo-opcion").value
     let val_horas = document.getElementById("input-modulo-horas").value
+    let val_fe = document.getElementById("input-modulo-fe").value
+    let val_fc = document.getElementById("input-modulo-fc").value
+    let val_ppp = document.getElementById("input-modulo-ppp").value
     if (modulo_id > 0) {
         ws_put(url_http + "modulo", {
             id: modulo_id,
             nombre: val_nombre,
             id_familia_profesional: val_familia,
             id_opcion_ocupacional: val_opcion,
-            horas: val_horas
+            horas: val_horas,
+            fe: (val_fe === "" ? "0" : val_fe),
+            fc: (val_fc === "" ? "0" : val_fc),
+            ppp: (val_ppp === "" ? "0" : val_ppp)
         }, respuestaRegistroModulo)
 
     } else {
@@ -27,7 +33,10 @@ var registrarModulo = () => {
             nombre: val_nombre,
             id_familia_profesional: val_familia,
             id_opcion_ocupacional: val_opcion,
-            horas: val_horas
+            horas: val_horas,
+            fe: val_fe,
+            fc: val_fc,
+            ppp: val_ppp
         }, respuestaRegistroModulo)
     }
 
@@ -40,9 +49,12 @@ var modificarModulo = () => {
         let datos = JSON.parse(sessionStorage.getItem("modulo"))
         modulo_id = datos.id_modulo
         document.getElementById("input-modulo-nombre").value = datos.nombre
-        document.getElementById("input-modulo-horas").value= datos.horas
+        document.getElementById("input-modulo-horas").value = datos.horas
+        document.getElementById("input-modulo-fe").value = datos.fe
+        document.getElementById("input-modulo-fc").value = datos.fc
+        document.getElementById("input-modulo-ppp").value = datos.ppp
         id_seleccion = datos.id_familia_profesional
-        id_seleccion_opcion = datos.id_opcion_ocupacional   
+        id_seleccion_opcion = datos.id_opcion_ocupacional
         consultarComunFamilia()
     } else {
         consultarComunFamilia()
